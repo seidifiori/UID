c1a6c01c66c89cab504ceef37f8e419524dd2ec2
Monogat.ari <ari.dellosso25@gmail.com>
2025-12-18 17:02:54 +0100

    Aggiunti sprites capelli maschili (ma per la femmina)

--- src/main/java/org/example/ProgettoUIDFinal/ClosetController.java
+++ src/main/java/org/example/ProgettoUIDFinal/ClosetController.java
@@ -168,7 +168,7 @@ public class ClosetController implements Initializable {
                 "cap1", "cap2", "cap3", "cap4", "cap5", "cap6",
                 "dres1", "dres2", "dres3", "dres4", "dres5", "dres6",
                 "armor1", "armor2", "armor3", "armor4",
-                "har1", "har2", "har3", "har4", "har5", "har6", "har7",
+                "har1", "har2", "har3", "har4", "har5", "har6", "har7", "har8", "har9",
                 "btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9"
         };
 
@@ -205,7 +205,6 @@ public class ClosetController implements Initializable {
                 // --- GESTIONE EQUIPAGGIAMENTO ---
                 int lastButtonIndex = 9;
                 if (type.startsWith("cap") || type.startsWith("dres") || type.startsWith("armor")) lastButtonIndex = 6;
-                else if (type.startsWith("har")) lastButtonIndex = 7;
 
                 // 1. Definiamo i Path e il NOME
                 String layerPath = null;
