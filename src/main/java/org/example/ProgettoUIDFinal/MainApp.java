ddb183c7f3fbaaffc30b31d517ee6a5aa90f29a2
Stocazzo <danieleddu04@gmail.com>
2026-01-11 12:31:23 +0100

    Aggiunto un eseguibile aggiornato,nessun update al codice

---
+++ src/main/java/org/example/ProgettoUIDFinal/MainApp.java
@@ -0,0 +1,10 @@
+package org.example.ProgettoUIDFinal;
+ // metti il tuo pacchetto reale
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Sostituisci 'App' con il nome della tua classe
+        // che contiene il metodo start() di JavaFX
+        Start.main(args);
+    }
+}
