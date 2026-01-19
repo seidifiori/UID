4727d2c402e4915ddbea7805771894f19debd468
Monogat.ari <ari.dellosso25@gmail.com>
2026-01-19 15:33:07 +0100

    aggiornamenti vari

--- src/main/java/org/example/ProgettoUIDFinal/MainApp.java
+++ src/main/java/org/example/ProgettoUIDFinal/MainApp.java
@@ -1,10 +1,7 @@
 package org.example.ProgettoUIDFinal;
- // metti il tuo pacchetto reale
 
 public class MainApp {
     public static void main(String[] args) {
-        // Sostituisci 'App' con il nome della tua classe
-        // che contiene il metodo start() di JavaFX
         Start.main(args);
     }
 }
